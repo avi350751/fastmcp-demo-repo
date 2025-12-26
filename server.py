6d4aa0e8b9d9069e964c3970b3e68e93b125316f
avi350751 <avishek350751@gmail.com>
2025-12-25 20:52:22 -0600

    updated server file

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from fastmcp import FastMCP
 
 from tools.math_tools import add_numbers, multiply_numbers, subtract_numbers
 from tools.text_tools import word_count
-from tools.misc_tools import random_quote, current_time
+from tools.misc_tools import random_quote, current_time, server_info
 
 mcp = FastMCP("math-mcp-server")
 
@@ -13,6 +13,8 @@ mcp.tool()(subtract_numbers)
 mcp.tool()(word_count)
 mcp.tool()(random_quote)
 mcp.tool()(current_time)
+mcp.resource("info://server")(server_info)
 
 if __name__ == "__main__":
-    mcp.run()
+    mcp.run(transport="http", host="0.0.0.0", port=8000) #streamable
+    #mcp.run()
